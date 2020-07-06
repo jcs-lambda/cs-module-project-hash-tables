9b6d34f873e2677d408363566d286223a923abff
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-07-06 18:36:02 -0500

    implements no_dups.py

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,29 @@
-def no_dups(s):
-    # Your code here
+def no_dups(s:str, max:int = 1000):
+    storage = [False] * max
+    first = True
+    last_index = len(s) - 1
+    output = ''
+    word = ''
+    hash = 14695981039346656037
 
+    for i, x in enumerate(s):
+        if x != ' ':
+            word += x
+            hash = hash * 1099511628211
+            hash = hash ^ ord(x)
+        if  x == ' ' or i == last_index:
+            index = hash % max
+            if not storage[index]:
+                if not first:
+                    output += ' '
+                else:
+                    first = not first
+                output += word
+                storage[index] = True
+            word = ''
+            hash = 14695981039346656037
+
+    return output        
 
 
 if __name__ == "__main__":
