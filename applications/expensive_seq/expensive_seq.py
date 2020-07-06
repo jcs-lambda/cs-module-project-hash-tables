992f80c6c421a8130e4cc57644631815bede8143
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-07-06 15:40:26 -0500

    implements expensive_seq.py

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,12 @@
-# Your code here
-
+lookup_table = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
-
+    if (x, y, z) not in lookup_table:
+        if x <= 0:
+            lookup_table[(x, y, z)] = y + z
+        else:
+            lookup_table[(x, y, z)] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+    return lookup_table[(x, y, z)]
 
 
 if __name__ == "__main__":
