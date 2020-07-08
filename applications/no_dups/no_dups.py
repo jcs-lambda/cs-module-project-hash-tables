3b522e673365c35c41ced01cb15784c31408cfa2
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-07-08 18:39:27 -0500

    refactors no_dups.py

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,27 +1,7 @@
-class Spacer:
-    """.get() method returns '' on first call, subsequent calls return ' '."""
-    def __init__(self):
-        self.get = self._blank
-    
-    def _blank(self) -> str:
-        self.get = self._space
-        return ''
-
-    def _space(self) -> str:
-        return ' '
-
+from collections import OrderedDict
 
 def no_dups(s:str):
-    seen = set()
-    output = ''
-    spacer = Spacer()
-
-    for word in s.split():
-        if word not in seen:
-            output = spacer.get().join([output, word])
-            seen.add(word)
-
-    return output        
+    return ' '.join(OrderedDict(zip(s.split(), s)).keys())
 
 
 if __name__ == "__main__":
